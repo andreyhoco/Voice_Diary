4f081a85af68a24c3c5626a00011797dc59bc71c
andreyhoco <andreygud2013@gmail.com>
2022-03-27 12:20:31 +0300

    Add RecordsFragment

--- app/src/main/java/com/designdrivendevelopment/voicediary/HostActivity.kt
+++ app/src/main/java/com/designdrivendevelopment/voicediary/HostActivity.kt
@@ -4,11 +4,18 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
 
 class HostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_host)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                replace(R.id.fragment_container, RecordsFragment.newInstance())
+            }
+        }
     }
 
     companion object {
---
+++ app/src/main/java/com/designdrivendevelopment/voicediary/RecordsFragment.kt
@@ -0,0 +1,21 @@
+package com.designdrivendevelopment.voicediary
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class RecordsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_records, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = RecordsFragment()
+    }
+}
