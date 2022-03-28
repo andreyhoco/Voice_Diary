c8f9b02e340f7d6d24b3fa98cf1132a6af9a8310
andreyhoco <andreygud2013@gmail.com>
2022-03-28 20:08:04 +0300

    Add time parsing to FileManagerImpl

--- app/src/main/java/com/designdrivendevelopment/voicediary/FileManagerImpl.kt
+++ app/src/main/java/com/designdrivendevelopment/voicediary/FileManagerImpl.kt
@@ -1,11 +1,15 @@
 package com.designdrivendevelopment.voicediary
 
 import android.content.Context
+
+import android.media.MediaMetadataRetriever
+import androidx.core.net.toUri
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 import java.io.File
 import java.io.IOException
-import java.util.Calendar
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FileManagerImpl(
     private val context: Context,
@@ -36,9 +40,21 @@ class FileManagerImpl(
 
     override suspend fun getRecords(): List<Record> = withContext(coroutineDispatcher) {
         return@withContext try {
-            recordsDir.listFiles { file ->
+            val calendar = Calendar.getInstance()
+            val currDay = with(calendar) {
+                set(Calendar.HOUR_OF_DAY, 0)
+                set(Calendar.MINUTE, 0)
+                set(Calendar.SECOND, 1)
+                time
+            }
+
+            val mediaMetadataRetriever = MediaMetadataRetriever()
+            val records = recordsDir.listFiles { file ->
                 file.canRead() && file.isFile && file.name.endsWith(FILE_EXTENSION)
-            }?.map { Record(it.name) } ?: emptyList()
+            }?.map { file -> file.toRecord(mediaMetadataRetriever, currDay, calendar) } ?: emptyList()
+
+            mediaMetadataRetriever.release()
+            records
         } catch (e: IOException) {
             emptyList()
         }
@@ -55,8 +71,51 @@ class FileManagerImpl(
         }
     }
 
+    private fun File.toRecord(
+        mediaMetadataRetriever: MediaMetadataRetriever,
+        currDay: Date,
+        calendar: Calendar
+    ): Record {
+        mediaMetadataRetriever.setDataSource(absolutePath)
+
+        val mediaMetadataDate = mediaMetadataRetriever
+            .extractMetadata(MediaMetadataRetriever.METADATA_KEY_DATE).orEmpty()
+        val date = SimpleDateFormat("yyyyMMdd'T'hhmmss.SSS'Z'", Locale.getDefault()).apply {
+            timeZone = TimeZone.getTimeZone("GMT")
+        }.parse(mediaMetadataDate)
+        val creationDate = if (date != null) {
+            calendar.time = date
+            val hours = calendar.get(Calendar.HOUR_OF_DAY).convertToTwoDigits()
+            val minutes = calendar.get(Calendar.MINUTE).convertToTwoDigits()
+            if (calendar.timeInMillis > currDay.time) {
+                "Сегодня в $hours:$minutes"
+            } else {
+                val year = calendar.get(Calendar.YEAR).convertToTwoDigits()
+                val month = calendar.get(Calendar.MONTH).convertToTwoDigits()
+                val day = calendar.get(Calendar.DAY_OF_MONTH).convertToTwoDigits()
+                "$day.$month.$year в $hours:$minutes"
+            }
+        } else {
+            ""
+        }
+
+        return Record(
+            name = name.dropLast(FILE_EXTENSION.length),
+            length = mediaMetadataRetriever
+                .extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)?.toLong()
+                ?: DEFAULT_LENGTH,
+            creationDate = creationDate,
+            uri = toUri()
+        )
+    }
+
+    private fun Int.convertToTwoDigits(): String {
+        return if (this in 0..9) "0$this" else this.toString()
+    }
+
     companion object {
         private const val DEFAULT_FILE_PREFIX = "record"
+        private const val DEFAULT_LENGTH = 0L
         private const val FILE_EXTENSION = ".mp3"
     }
 }
--- app/src/main/java/com/designdrivendevelopment/voicediary/Record.kt
+++ app/src/main/java/com/designdrivendevelopment/voicediary/Record.kt
@@ -1,3 +1,10 @@
 package com.designdrivendevelopment.voicediary
 
-data class Record(val name: String)
+import android.net.Uri
+
+data class Record(
+    val name: String,
+    val length: Long,
+    val creationDate: String,
+    val uri: Uri
+)
